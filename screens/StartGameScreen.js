c0e47cc98362e8895cf65eb5a3e499b4ba64ef8a
mr7aali <mr7aali@gmail.com>
2024-02-20 01:45:49 +0600

    elevation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -15,9 +15,12 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
         marginTop: 100,
+        marginHorizontal: 24,
         padding: 16,
-        backgroundColor: "#72063c"
+        backgroundColor: "#72063c",
+        borderRadius:8,
+        elevation:4,
+        shadowColor:"black"
     }
 });
